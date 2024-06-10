3d67725c15fd9e42eff7c88520a6ea722fa26e65
Mohamed Abdelmaksoud <medohmada1623@gmail.com>
2024-06-10 17:57:11 +0300

    added date in appointment

--- components/Appointment.tsx
+++ components/Appointment.tsx
@@ -5,29 +5,33 @@ import StatusSelect from "./StatusSelect";
 export default function Appointment({
   data,
   onEdit,
-  onDelete
+  onDelete,
 }: {
   data: {
     id: string;
     clientName: string;
     services: string[];
+    date: string;
     time: string;
     status: string;
   };
   onEdit: () => void;
   onDelete: () => void;
 }) {
+  const thisDate = new Date(data.date);
+  const formattedDate = `${thisDate.getDate()} ${thisDate.toLocaleDateString(
+    "en-US",
+    {
+      month: "long",
+    },
+  )}, ${thisDate.getFullYear()}`;
   return (
     <div className="flex flex-col gap-4 rounded-lg border border-[#F1F5F9] py-5">
       <div className="flex items-center justify-between px-4">
         <h4 className="text-xs font-semibold text-black">{data.clientName}</h4>
-        <div className="flex gap-2 items-center">
-          <StatusSelect 
-            intialValue={data.status}
-            key={data.id}
-          />
-          <DotsMenu onDelete={onDelete} onEdit={onEdit}  />
-
+        <div className="flex items-center gap-2">
+          <StatusSelect intialValue={data.status} key={data.id} />
+          <DotsMenu onDelete={onDelete} onEdit={onEdit} />
         </div>
       </div>
       <hr className="border-[#F1F5F9]" />
@@ -36,6 +40,14 @@ export default function Appointment({
           <h4 className="text-xs font-medium text-textGray">الخدمة </h4>
           <h5 className="text-xs font-semibold text-black">
             {data.services.join(", ")}
+          </h5>
+        </div>
+        <div className="flex items-center justify-between">
+          <h4 className="text-xs font-medium text-textGray">اليوم </h4>
+          <h5
+            className={`${english_font.className} text-xs font-semibold text-black`}
+          >
+            <bdi>{formattedDate}</bdi>
           </h5>
         </div>
         <div className="flex items-center justify-between">
