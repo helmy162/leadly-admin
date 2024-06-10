94207b32067652cfe024a842de85f071296a3476
Mohamed Abdelmaksoud <medohmada1623@gmail.com>
2024-06-10 15:42:58 +0300

    added EN button

--- app/Header.tsx
+++ app/Header.tsx
@@ -3,8 +3,10 @@ import Image from "next/image";
 
 export default function Header() {
   return (
-    <header className="w-full pb-5">
+    <header className="flex w-full justify-between pb-5 px-4">
+      <div className="w-6" />
       <Logo className="mx-auto" />
+      <button className="text-sm font-bold text-[#9B9B9B] w-6">EN</button>
     </header>
   );
 }
