1e1d7a08fa733c9a4f4bdb9e66cbea2887b2d4c4
Mohamed Abdelmaksoud <medohmada1623@gmail.com>
2024-06-11 17:21:03 +0300

    hide language switch button for now

--- app/Header.tsx
+++ app/Header.tsx
@@ -4,9 +4,9 @@ import Image from "next/image";
 export default function Header() {
   return (
     <header className="flex w-full justify-between pb-5 px-4">
-      <div className="w-6" />
+      {/* <div className="w-6" /> */}
       <Logo className="mx-auto" />
-      <button className="text-sm font-bold text-[#9B9B9B] w-6">EN</button>
+      {/* <button className="text-sm font-bold text-[#9B9B9B] w-6">EN</button> */}
     </header>
   );
 }
